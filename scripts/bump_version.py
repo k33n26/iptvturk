c6f2609132bd745f240d66d4ead1bb9f1f05406f
k33n26 <37938131+k33n26@users.noreply.github.com>
2026-01-25 18:07:53 +0300

    Update bump_version.py

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -1,8 +1,6 @@
 v = open("VERSION.txt").read().strip()
 major, minor, patch = map(int, v.split("."))
-
 patch += 1
 new = f"{major}.{minor}.{patch}"
-
 open("VERSION.txt", "w").write(new)
 print(new)
